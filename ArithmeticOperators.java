71a26486f9821859259821dec8bba72a17c714b2
Pratusha Pandey <pratushapandey08@gmail.com>
2022-08-07 01:50:20 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+class ArithmeticOperators{
+    public static void main(String[]args)
+    {
+        int a = 10;
+        int b = 20;
+        int c= 30;
+        System.out.println("a + b +c : " + (a+b+c));
+        System.out.println("a - b  : " + ( a- b));
+        System.out.println("a * b * c : " + (a*b*c));
+        System.out.println("a / b : "+(b/a));
+    }
+}
